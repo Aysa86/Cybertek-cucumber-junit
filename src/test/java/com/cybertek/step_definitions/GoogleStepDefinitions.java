0a0381d8c75c011f252e631733c0a242de0c2c5a
Aysa Dzhalaeva <adzhalaeva@gmail.com>
2020-07-10 16:52:57 -0400

    update

--- src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
@@ -1,20 +1,32 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class GoogleStepDefinitions {
     @Given("User is on the Google search page")
     public void user_is_on_the_google_search_page() {
-        System.out.println("USER IS ON THE GOOGLE SEARCH PAGE");
+
+        Driver.getDriver().get("https://www.google.com");
+
+        // Assert.fail("Unpurposed failing here!!!");
+        // if first test fails, the rest tests will not be checked
     }
 
 
-    //Some other steps were also undefined:
 
     @Then("User should see title is Google")
     public void user_should_see_title_is_google() {
-        System.out.println("USER SEES THE TITLE IS GOOGLE");
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Google";
+
+
+
+        Assert.assertEquals("Actual title doesn't match expected title!", actualTitle, expectedTitle);
+       // Assert.assertTrue("Actual title doesn't match expected title!", actualTitle.equals(expectedTitle));
     }
+
 
 }
