25801101b337ddebcac36391e6301d3b17ee530c
Aysa Dzhalaeva <adzhalaeva@gmail.com>
2020-07-13 12:35:10 -0400

    update

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -8,7 +8,13 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
-        glue = "com/cybertek/step_definitions"
+        glue = "com/cybertek/step_definitions",
+        dryRun = false,
+        tags = "@login"
+
+
+
+
 )
 
 public class CukesRunner {
--- target/test-classes/features/Search.feature
+++ target/test-classes/features/Search.feature
@@ -1,7 +1,7 @@
 Feature: Search
   Agile Story: As a user I should be able to search when I'm on Google search page.
 
-  Scenario: Google default
+  Scenario: Google default title verification
     Given User is on the Google search page
     Then User should see title is Google
 
