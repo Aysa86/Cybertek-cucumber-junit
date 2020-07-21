7e63de1f9a9e0326666f0de517eb63f5b403c6bc
Aysa Dzhalaeva <adzhalaeva@gmail.com>
2020-07-21 15:41:27 -0400

    update

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Driver {
 
@@ -37,6 +41,26 @@ public class Driver {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
                     break;
+                case "chrome-remote":
+                    try {
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        URL url = new URL("http://54.162.211.172:4444/wd/hub");
+                        driver = new RemoteWebDriver(url, chromeOptions);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "firefox-remote":
+                    try {
+                        // to request Selenium Grid to run tests on Firefox
+                        FirefoxOptions firefoxOptions = new FirefoxOptions();
+                        URL url = new URL("http://54.162.211.172:4444/wd/hub");
+                        driver = new RemoteWebDriver(url, firefoxOptions);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                default:
+                    throw new RuntimeException("Wrong browser name :: " + browser);
             }
         }
         return driver;
